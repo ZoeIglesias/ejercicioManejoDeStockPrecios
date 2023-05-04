30e693b09c0be37f0e251a4a25dd0e1d21dcc8a3
ZoeIglesias <ziglesias@frba.utn.edu.ar>
2023-05-04 18:20:57 -0300

    Agrego funcion calculo de stock junto con su test

--- src/main/java/productos/Combo.java
+++ src/main/java/productos/Combo.java
@@ -1,9 +1,6 @@
 package productos;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
+import java.util.*;
 
 
 public class Combo extends Producto{
@@ -18,8 +15,8 @@ public class Combo extends Producto{
     }*/
     //Con parametros variables
 
-   public void agregarProductos(ProductoSimple ... nuevoProducto){
-       Collections.addAll(this.productos,nuevoProducto);
+    public void agregarProductos(ProductoSimple ... nuevoProducto){ //recibe un array primitivo
+        Collections.addAll(this.productos,nuevoProducto);
     }
 
     @Override
@@ -29,10 +26,21 @@ public class Combo extends Producto{
     //"Los objetos de las colecciones no pertenezcan a las colecciones sino a un objeto llamado stream"
 
     @Override
-    public int stock(){
-        return 0;
+    public int stock(){                                                //Producto::stock es un llamado a metodo
+        return Collections.min(this.productos, Comparator.comparing(Producto::stock)).stock();
+        //llamamos a la clase collections al metodo min que tiene dos parametros:
+        //-la coleccion donde queresmo encontrar el minimo
+        //-la forma de comprarlos (queremos compararlos segun el stock) entonces llamamos a la clase producto el metodo stock
+        //nos devuleve el producto por lo que le preguntamos el stock del mismo
+    }
+
+    //como lo pense yo
+    public int stock2(){
+        OptionalInt minStock = this.productos.stream().mapToInt(producto -> producto.stock()).min();
+        return  minStock.orElse(0);
     }
 
 
 
 }
+
--- src/test/java/TestVarios.java
+++ src/test/java/TestVarios.java
@@ -1,21 +1,40 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import productos.Combo;
 import productos.ProductoSimple;
 
 public class TestVarios {
 
-    @Test
-    public void testDePreciosDeUnCombo(){
-        ProductoSimple casco = new ProductoSimple(300.0,5);
-        ProductoSimple guante = new ProductoSimple(100.0,2);
+    ProductoSimple casco;
+    ProductoSimple guante;
+    Combo comboSimple;
+    Combo comboDoble;
 
-        Combo comboSimple = new Combo();
-
+    @BeforeEach
+    public void init(){
+        this.casco = new ProductoSimple(300.0,5);
+        this.guante = new ProductoSimple(100.0,2);
+        this.comboSimple = new Combo();
+        this.comboDoble = new Combo();
         comboSimple.agregarProductos(casco,guante);
 
+    }
+
+    @Test
+    @DisplayName("Prueba de el precio de un combo.")
+    public void testDePrecioDeUnComboVale400(){
+
         Assertions.assertEquals(400.0,comboSimple.precio());
+
+    }
+
+    @Test
+    @DisplayName("Prueba de stock de un combo.")
+    public void testsDeStockDeUnComboVale2(){
+
+        Assertions.assertEquals(2,comboSimple.stock());
 
     }
 }
